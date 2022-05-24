b7fde7747f80ead92035b21d822d120d26609f4e
superstark01 <superstark02@gmail.com>
2022-05-24 23:44:11 +0530

    123

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Task1 from './Pages/Task1';
-import Task2 from './Pages/Task2';
 import Home from './Pages/Home';
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/task1" element={<Task1 />} />
-          <Route path="/task2" element={<Task2 />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,90 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
-class Home extends Component {
-    render() {
-        return (
-            <div style={{
-                width: "100%",
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: 'center',
-                gap: "10px"
-            }} >
-                <div>
-                    <Link to="/task1" >Task 1</Link>
-                </div>
-                <div>
-                    <Link to="/task2" >Task 2</Link>
-                </div>
-            </div>
-        );
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script')
+    script.src = src
+    script.onload = () => {
+      resolve(true)
     }
+    script.onerror = () => {
+      resolve(false)
+    }
+    document.body.appendChild(script)
+  })
 }
 
-export default Home;
+function App() {
+  async function postData() {
+    const data = await fetch('https://api.nimbbl.tech/api/v2/generate-token', {
+      method: 'POST',
+      body: JSON.stringify({ "access_key": "access_key_x1DveRbWzVmWW3KB", "access_secret": "access_secret_1DvejDD1MqoWW3KB" }),
+      headers: { 'Content-Type': 'application/json' }
+    }).then((t) =>
+      t.json()
+    )
+
+    const created_order = await fetch('https://api.nimbbl.tech/api/v2/create-order', {
+      method: 'POST',
+      body: JSON.stringify({
+        "amount_before_tax": 4,
+        "currency": "INR",
+        "invoice_id": "BrQv9nkxDEqWR3zg1122222",
+        "tax": 0,
+        "user": {
+          "mobile_number": "9999999999",
+          "email": "rakesh.kumar@example.com",
+          "first_name": "Rakesh", "last_name": "Kumar"
+        },
+        "shipping_address": {
+          "address_1": "Some address",
+          "street": "Your street",
+          "landmark": "My landmark",
+          "area": "My area",
+          "city": "Mumbai",
+          "state": "Maharashtra",
+          "pincode": "400018",
+          "address_type": "residential"
+        },
+        "total_amount": 4,
+      }),
+      headers: {
+        'Authorization': 'Bearer ' + data.token,
+        'Content-Type': 'application/json'
+      },
+    }).then((t) =>
+      t.json()
+    )
+
+    const res = await loadScript('https://api.nimbbl.tech/static/assets/js/checkout.js')
+    if (!res) {
+      alert('SDK failed to load. Are you online?')
+      return
+    }
+
+    var options = {
+      "access_key": "access_key_x1DveRbWzVmWW3KB", // Enter the Key ID generated from the Dashboard
+      "order_id": created_order.order_id, // Enter the order_id from the create-order api
+      "callback_handler": function (response) {
+        console.log(response.nimbbl_transaction_id);
+        console.log(response.nimbbl_order_id);
+        console.log(response.nimbbl_signature);
+        console.log(response.status);
+      },
+      "custom": {
+        "key_1": "val_1",
+        "key_2": "val_2"
+      },
+    };
+
+    const paymentObject = new window.NimbblCheckout(options)
+    paymentObject.open(created_order.order_id)
+  }
+
+  return (
+    <div className="card">
+      <button className="btn btn-sm btn-primary" onClick={postData}>Pay Now</button>
+    </div>
+  );
+}
+export default App;
